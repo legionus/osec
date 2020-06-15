legionus/osec

commit 804cbfe8d952fb603598f699579e785eb19e9191

Drop global buffer
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

src/dbvalue.c
@@ -9,6 +9,7 @@
  */
 #include "config.h"
 
+#include <sys/user.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,9 +23,6 @@
 #include <gcrypt.h>
 
 #include "osec.h"
-
-void *read_buf;
-size_t read_bufsize;
 
 void *osec_field(const unsigned type, const void *data, const size_t dlen,
 		struct field *ret)
@@ -136,6 +134,7 @@ bool osec_digest(struct record *rec, const char *fname,
 	gcry_md_hd_t handle = NULL;
 	unsigned char *data_ptr;
 	bool ret, retval = false;
+	char read_buf[PAGE_SIZE];
 
 	struct record local_rec = { 0 };
 
@@ -168,9 +167,8 @@ bool osec_digest(struct record *rec, const char *fname,
 	/* Let the kernel know we are going to read everything in sequence. */
 	(void) posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
 
-	while ((num = read(fd, read_buf, read_bufsize)) > 0) {
+	while ((num = read(fd, read_buf, sizeof(read_buf))) > 0)
 		gcry_md_write(handle, read_buf, (size_t) num);
-	}
 
 	if (num == -1) {
 		osec_error("read: %s: %m", fname);

src/osec.c
@@ -27,10 +27,6 @@
 
 #include "osec.h"
 
-// Global variables
-extern void *read_buf;
-extern size_t read_bufsize;
-
 size_t pw_bufsize = 0;
 size_t gr_bufsize = 0;
 
@@ -612,15 +608,6 @@ end:
 
 static void allocate_globals(void)
 {
-	// Allocate buffer to read the files (digest.c).
-	read_bufsize = (size_t)(sysconf(_SC_PAGE_SIZE) - 1);
-	read_buf = malloc(read_bufsize);
-
-	if (!read_buf) {
-		osec_error("malloc: %m");
-		exit(EXIT_FAILURE);
-	}
-
 	// (status.c)
 	long val;
 
@@ -795,7 +782,6 @@ int main(int argc, char **argv)
 			retval = EXIT_FAILURE;
 	}
 
-	free(read_buf);
 	free(exclude_matches);
 
 	return retval;

update/updatedb.c
@@ -21,9 +21,6 @@
 
 #include "osec.h"
 
-void *read_buf;
-size_t read_bufsize;
-
 // FIXME: use config file for this variables.
 char def_db_path[] = "/tmp/osec";
 char *db_path = NULL;
@@ -234,15 +231,6 @@ main(int argc, char **argv)
 
 	if (cdb_make_start(&cdbn, fdtemp) < 0)
 		osec_fatal(EXIT_FAILURE, errno, "cdb_make_start");
-
-	// Allocate buffer for reading files.
-	read_bufsize = (size_t)(sysconf(_SC_PAGE_SIZE) - 1);
-	read_buf = malloc(read_bufsize);
-
-	if (!read_buf) {
-		osec_error("malloc: %m");
-		exit(EXIT_FAILURE);
-	}
 
 	/*
 	 * Set default data buffer. This value will increase in the process of

commit 0ca40d8240ea809777bb734d534f1fb0dfeaf523

Use BUFSIZ for read
It is incorrect to use PAGE_SIZE since this constant exists on some
architectures. Page size can be different even on the same architecture.

Fixes: 804cbfe ("Drop global buffer")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

src/dbvalue.c
@@ -129,7 +129,7 @@ bool osec_digest(struct record *rec, const char *fname)
 	gcry_md_hd_t handle = NULL;
 	unsigned char *data_ptr;
 	bool ret, retval = false;
-	char read_buf[PAGE_SIZE];
+	char read_buf[BUFSIZ];
 
 	struct record local_rec = { 0 };
 